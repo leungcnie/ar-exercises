2e6579ffc0510cf014ada4e4945a5f7b33c964cb
Connie <leungcnie@gmail.com>
2021-01-04 22:59:25 +0000

    Connect Employee and Store models and add employees data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# Add data into employees
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sandy", last_name: "Cheeks", hourly_rate: 50)
+@store2.employees.create(first_name: "Spongebob", last_name: "Squarepants", hourly_rate: 20)
+@store2.employees.create(first_name: "Squidward", last_name: "Tentacles", hourly_rate: 30)
