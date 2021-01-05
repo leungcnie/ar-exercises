e59d1b934ae8ea065a0c89f19ec5dafa8ba13515
Connie <leungcnie@gmail.com>
2021-01-05 00:47:27 +0000

    Add validation to Employee and Store models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name"
+store_name = $stdin.gets.chomp
+store = Store.new
+store.name = store_name
+store.annual_revenue = 1
+store.mens_apparel = false
+store.womens_apparel = true
+store.save!
+
+puts store.errors.full_messages
