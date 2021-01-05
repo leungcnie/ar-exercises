5d8fbc9214be026dcc95ca3a2b841695d0b9a772
Connie <leungcnie@gmail.com>
2021-01-05 00:47:01 +0000

    Update employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,5 +12,5 @@ puts "----------"
 # Add data into employees
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Sandy", last_name: "Cheeks", hourly_rate: 50)
-@store2.employees.create(first_name: "Spongebob", last_name: "Squarepants", hourly_rate: 20)
-@store2.employees.create(first_name: "Squidward", last_name: "Tentacles", hourly_rate: 30)
+@store2.employees.create(first_name: "Spongebob", last_name: "Squarepants", hourly_rate: 40)
+@store2.employees.create(first_name: "Squidward", last_name: "Tentacles", hourly_rate: 50)
